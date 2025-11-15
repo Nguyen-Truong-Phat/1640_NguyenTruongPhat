ccf88b70dfc2b6d7cd72748e29cc7d4cb5888e79
Phat <tinal27725@gmail.com>
2025-11-15 17:49:55 +0700

    upload BTVN Tuan 1

--- BTVNTuan1.cs
+++ BTVNTuan1.cs
@@ -4,92 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bai1
+namespace BTVN1
 {
-    class GiaiPTBac1
+    class Bac1
     {
-        protected double a, b;
-
-        public GiaiPTBac1(double a, double b)
+        protected float a, b;
+        public Bac1(float a, float b)
         {
             this.a = a;
             this.b = b;
         }
-
-        public virtual void Giai()
+        public virtual void implement()
         {
-            if (a == 0)
+            if(a == 0)
             {
-                if (b == 0)
-                    Console.WriteLine("Phương trình vô số nghiệm.");
-                else
-                    Console.WriteLine("Phương trình vô nghiệm.");
+                Console.WriteLine("Phương trình vô nghiệm");
             }
             else
             {
-                double x = -b / a;
-                Console.WriteLine("Nghiệm phương trình bậc 1: x = " + x);
+                float x = -b / a;
+                Console.WriteLine("Nghiệm của phương trình là: x = " + x);
             }
         }
     }
-
-    class GiaiPTBac2 : GiaiPTBac1
+    class Bac2 : Bac1
     {
-        private double c;
-
-        public GiaiPTBac2(double a, double b, double c) : base(a, b)
+        private float c;
+        public Bac2(float a, float b, float c) : base(a, b)
         {
             this.c = c;
         }
-
-        public override void Giai()
+        public override void implement()
         {
             if (a == 0)
             {
-                // Trường hợp trở thành phương trình bậc 1
-                base.Giai();
-                return;
-            }
-
-            double delta = b * b - 4 * a * c;
-
-            if (delta > 0)
-            {
-                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
-                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
-                Console.WriteLine("Phương trình có 2 nghiệm phân biệt:");
-                Console.WriteLine("x1 = " + x1);
-                Console.WriteLine("x2 = " + x2);
-            }
-            else if (delta == 0)
-            {
-                double x = -b / (2 * a);
-                Console.WriteLine("Phương trình có nghiệm kép: x1 = x2 = " + x);
+                Bac1 pt1 = new Bac1(b, c);
+                Console.Write("Phương trình trở thành bậc 1: ");
+                pt1.implement();
             }
             else
             {
-                Console.WriteLine("Phương trình vô nghiệm thực.");
+                float delta = (b * b) - (4 * a * c);
+                if (delta < 0)
+                    Console.WriteLine("Phương trình vô nghiệm.");
+                else if (delta == 0)
+                {
+                    float x = -b / (2 * a);
+                    Console.WriteLine("Phương trình có nghiệm kép: x1 = x2 = " + x);
+                }
+                else
+                {
+                    double x1 = ((-b + Math.Sqrt(delta)) / (2 * a));
+                    double x2 = ((-b - Math.Sqrt(delta)) / (2 * a));
+                    Console.WriteLine("Phương trình có hai nghiệm phân biệt:\n x1 = " + x1 + "\n x2 = " + x2);
+                }
             }
         }
     }
-
-    internal class Program
+    internal class BTVNTuan1
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            
             Console.WriteLine("Giải phương trình bậc 2: ax^2 + bx + c = 0");
+            Console.Write("Nhập liệu cho biến a: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Nhập liệu cho biến b: ");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("Nhập liệu cho biến c: ");
+            float c = float.Parse(Console.ReadLine());
+            var ptb2 = new Bac2(a,b,c);
+            ptb2.implement();
 
-            Console.Write("Nhập a: ");
-            double a = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Nhập b: ");
-            double b = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Nhập c: ");
-            double c = Convert.ToDouble(Console.ReadLine());
-
-            GiaiPTBac2 pt = new GiaiPTBac2(a, b, c);
-            pt.Giai();
+            Console.ReadKey();
         }
     }
 }

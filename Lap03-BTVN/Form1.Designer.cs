f3f24c1d33ecd9a492832d83d5c67316490e5ee3
Trường Phát <161675671+Nguyen-Truong-Phat@users.noreply.github.com>
2025-11-28 20:44:35 +0700

    Add files via upload

---
+++ Lap03-BTVN/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Lap03_BTVN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvStudents = new System.Windows.Forms.ListView();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvStudents
+            // 
+            this.lvStudents.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.LastName,
+            this.FirstName,
+            this.Phone});
+            this.lvStudents.FullRowSelect = true;
+            this.lvStudents.GridLines = true;
+            this.lvStudents.HideSelection = false;
+            this.lvStudents.Location = new System.Drawing.Point(16, 57);
+            this.lvStudents.Name = "lvStudents";
+            this.lvStudents.Size = new System.Drawing.Size(418, 237);
+            this.lvStudents.TabIndex = 0;
+            this.lvStudents.UseCompatibleStateImageBehavior = false;
+            this.lvStudents.View = System.Windows.Forms.View.Details;
+            this.lvStudents.SelectedIndexChanged += new System.EventHandler(this.lvStudents_SelectedIndexChanged);
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(440, 57);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(100, 23);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name";
+            this.lblLastName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(440, 135);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(100, 23);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(440, 216);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(100, 23);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Phone";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(443, 83);
+            this.txtLastName.Multiline = true;
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(238, 31);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(443, 161);
+            this.txtFirstName.Multiline = true;
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(238, 31);
+            this.txtFirstName.TabIndex = 5;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(443, 242);
+            this.txtPhone.Multiline = true;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(238, 31);
+            this.txtPhone.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(556, 293);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(125, 37);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add Name";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Last Name";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 140;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Phone";
+            this.columnHeader3.Width = 100;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.lvStudents);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.lblLastName);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.lblFirstName);
+            this.groupBox1.Controls.Add(this.txtFirstName);
+            this.groupBox1.Controls.Add(this.lblPhone);
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Location = new System.Drawing.Point(42, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(717, 392);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detail";
+            // 
+            // LastName
+            // 
+            this.LastName.Text = "Last Name";
+            this.LastName.Width = 110;
+            // 
+            // FirstName
+            // 
+            this.FirstName.Text = "First Name";
+            this.FirstName.Width = 130;
+            // 
+            // Phone
+            // 
+            this.Phone.Text = "Phone";
+            this.Phone.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "List View Demo";
+            this.Load += new System.EventHandler(this.LVdemo_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvStudents;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ColumnHeader LastName;
+        private System.Windows.Forms.ColumnHeader FirstName;
+        private System.Windows.Forms.ColumnHeader Phone;
+    }
+}
+
---
+++ Lap03-BTVN/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lap03_BTVN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string lastName = txtLastName.Text.Trim();
+            string firstName = txtFirstName.Text.Trim();
+            string phone = txtPhone.Text.Trim();
+
+            if (string.IsNullOrEmpty(lastName) ||
+                string.IsNullOrEmpty(firstName) ||
+                string.IsNullOrEmpty(phone))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ Last Name, First Name và Phone.");
+                return;
+            }
+
+            // Tạo 1 dòng mới cho ListView
+            ListViewItem item = new ListViewItem(lastName);  // cột 1
+            item.SubItems.Add(firstName);                    // cột 2
+            item.SubItems.Add(phone);                        // cột 3
+
+            // Thêm vào ListView
+            lvStudents.Items.Add(item);
+
+            // Xóa textbox sau khi thêm
+            txtLastName.Clear();
+            txtFirstName.Clear();
+            txtPhone.Clear();
+            txtLastName.Focus();
+        }
+
+        
+
+        private void lvStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvStudents.SelectedItems.Count == 0)
+                return;
+
+            ListViewItem item = lvStudents.SelectedItems[0];
+
+            txtLastName.Text = item.SubItems[0].Text;
+            txtFirstName.Text = item.SubItems[1].Text;
+            txtPhone.Text = item.SubItems[2].Text;
+        }
+
+        private void LVdemo_Load(object sender, EventArgs e)
+        {
+            ListViewItem item1 = new ListViewItem("Ly");   // Last Name
+            item1.SubItems.Add("Thi Bong");                // First Name
+            item1.SubItems.Add("23456");                   // Phone
+            lvStudents.Items.Add(item1);
+
+            // Học sinh 2
+            ListViewItem item2 = new ListViewItem("Nguyen");
+            item2.SubItems.Add("Van Chinh");
+            item2.SubItems.Add("4555");
+            lvStudents.Items.Add(item2);
+
+            // Học sinh 3
+            ListViewItem item3 = new ListViewItem("Tran");
+            item3.SubItems.Add("Chanh Truc");
+            item3.SubItems.Add("123456");
+            lvStudents.Items.Add(item3);
+        }
+    }
+}
